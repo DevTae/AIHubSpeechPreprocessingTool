d1119b8b3ac3df0a03dc5279e166dd0821eaf595
DevTae <dev.taehyeon.kim@gmail.com>
2023-09-11 19:22:57 +0900

    Update preprocess.sh to add SAVE_PATH option

--- preprocess/preprocess.sh
+++ preprocess/preprocess.sh
@@ -3,6 +3,7 @@
 
 DATASET_PATH="/media/milab/SSD1/"		# metadata.txt 와 DATASET_PATH
 VOCAB_DEST='/media/milab/SSD1/'			# 위와 동일하게 세팅 진행
+SAVE_PATH='/media/milab/SSD1/'			# 위와 동일하게 세팅 진행
 OUTPUT_UNIT='ipa'                                          # you can set character / ipa / subword / grapheme
 PREPROCESS_MODE='phonetic'                                       # phonetic : 칠 십 퍼센트,  spelling : 70%
 VOCAB_SIZE=5000                                                  # if you use subword output unit, set vocab size
@@ -12,6 +13,7 @@ echo "Pre-process Speech Dataset.."
 python main.py \
 --dataset_path $DATASET_PATH \
 --vocab_dest $VOCAB_DEST \
+--savepath $SAVE_PATH \
 --output_unit $OUTPUT_UNIT \
 --preprocess_mode $PREPROCESS_MODE \
 --vocab_size $VOCAB_SIZE \
